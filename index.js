561e1d8106e9aca306a32a2f07293f5de4fe7e7c
Ashwin <ashwinis395@gmail.com>
2023-04-15 01:03:50 +0530

    Add associations CRUD

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const init = async () => {
 
   const server = Hapi.server({
     port: 3000,
-    host: 'localhost'
+    host: '127.0.0.1'
   });
 
   await server.register({
@@ -21,6 +21,8 @@ const init = async () => {
     }
   });
 
+
+  //*******************************************************USERS******************************************************************//
   // Get all users
   server.route({
     method: 'GET',
@@ -37,7 +39,6 @@ const init = async () => {
   });
 
   // Add a new user to the database
-
   server.route({
     method: 'POST',
     path: '/users',
@@ -103,6 +104,73 @@ const init = async () => {
       handler: (req, h) => {
 
           return 'Return search results for the specified term';
+      }
+  });
+
+  //*************************************************WELFARE ASSOCIATION******************************************************************//
+
+  // Add a new welfare association to the database
+  server.route({
+    method: 'POST',
+    path: '/associations',
+    handler: async (req, h) => {
+      let payload = req.payload
+      payload.isDelete = false
+      try{
+        const user = await req.mongo.db.collection('associations').insertOne(payload);
+        return {status: true, data:user, message: "Add new association."};
+      }catch(err){
+        return {status: false, data:{}, message: err.message};
+      }
+      
+    }
+  });
+
+  // Get all associations
+  server.route({
+    method: 'GET',
+    path: '/associations',
+    handler: async (req, h) => {
+      const offset = Number(req.query.offset) || 0;
+      try{
+        const associations = await req.mongo.db.collection('associations').find({}).sort({metacritic:-1}).skip(offset).limit(20).toArray();
+        return {status: true, data:associations, message: "Get all associations"};
+      }catch(err){
+        return {status: false, data:[], message: "Something went wrong"};
+      }
+    }
+  });
+
+  // Update the details of a association
+  server.route({
+    method: 'PUT',
+      path: '/associations/{id}',
+      handler: async (req, h) => {
+        const id = req.params.id
+        const ObjectID = req.mongo.ObjectID;
+        const payload = req.payload
+        try{
+          const user = await req.mongo.db.collection('associations').findOneAndUpdate({_id: ObjectID(id)}, {$set:{association_name:payload.association_name}}, {new: true})
+          return {status: true, data:user, message: "Update association"};
+        }catch(err){
+          return {status: false, data:{}, message: "Something went wrong."};
+        }
+      }
+  });
+
+  // Delete a association from the database
+  server.route({
+      method: 'DELETE',
+      path: '/associations/{id}',
+      handler: async (req, h) => {
+        const id = req.params.id
+        const ObjectID = req.mongo.ObjectID;
+        try{
+          const data = await req.mongo.db.collection('associations').deleteOne({_id: ObjectID(id)});
+          return {status: true, data:data, message: "Delete association data"};
+        }catch(err){
+          return {status: false, data:{}, message: "Something went wrong."};
+        }
       }
   });
 
